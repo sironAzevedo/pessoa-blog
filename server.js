d3a460733ca444c48864873b059765a2cd058acc
sironAzevedo <sirondba@gmail.com>
2018-03-23 18:47:38 -0300

    Refatorando

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8000);
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
 
-console.log('Console listening!');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + 'dist/index.html'));
+});
+
+const port = process.env.PORT || '8000';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Console listening!'));
